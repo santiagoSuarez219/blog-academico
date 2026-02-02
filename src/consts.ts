755b6780e51eed51997426cb10ed7ef516b4f273
santiagoSuarez219 <santiago8628@gmail.com>
2026-02-02 10:21:28 -0500

    feat: actualizar contenido del pie de página y mejorar la descripción del blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Blog";
+export const SITE_TITLE = "🚀 Blog";
 export const SITE_DESCRIPTION =
   "Blog de clases de programación y ciencias de la computación.";
 
 export const CATEGORIES = [
   { id: "estructura-de-datos", label: "Estructura de datos" },
   { id: "programacion-cientifica", label: "Programación científica" },
+  { id: "analisis-algoritmos", label: "Análisis de algoritmos" },
   { id: "inteligencia-artificial", label: "Inteligencia artificial" },
+  
 ];
